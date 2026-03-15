2cb91f21c44faada2aaf22a8f7595ea7e2e485b8
iTx Sarkar <project01bybandaheali@gmail.com>
2026-03-16 04:52:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-//Powered By Team-Bandaheali
-const serverfile = "index.js";
+const smd = require("team-bandaheali");
+const connectDb = require("./data/smd-mini");
+const msg = require("./lib/msg");
+
+async function startBot() {
+  await connectDb();
+
+  const smdMini = smd.getfiles();
+  await smdMini.start();
+
+  msg();
+}
+
+startBot();
